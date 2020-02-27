d23d8770587e39bb575ace9fc4a133aadc28f401
bexon26 <47786103+bexon26@users.noreply.github.com>
2020-02-27 14:05:58 +0300

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    var String = n.toString();
+    var mass = String.split("");
+    mass.reverse();
+    var reverseString = mass.join('');
+    var reverseNumber = parseInt(reverseString, 10);
+    return reverseNumber;
 }
